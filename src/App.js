7186e36541e2fbc6825c193e46f4e8b4eebff04f
Hxpere-2.0 <108596550+AmuinExplorer@users.noreply.github.com>
2024-03-17 20:41:25 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Register from './components/Register';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
         {/* <Route path='/register' element={} /> */}
       </Routes>
     </Router>
